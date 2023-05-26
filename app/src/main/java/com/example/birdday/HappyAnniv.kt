8e94c5a0b1b282f3683e7c5b27ab1ef1452a24b9
najwansv <najwan.joan.sv@gmail.com>
2023-05-26 08:10:47 +0700

    update anniv

--- app/src/main/java/com/example/birdday/HappyAnniv.kt
+++ app/src/main/java/com/example/birdday/HappyAnniv.kt
@@ -1,11 +1,48 @@
 package com.example.birdday
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
 
 class HappyAnniv : AppCompatActivity() {
+    private var song = SongStorage(this)
+    private lateinit var player: ExoPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_happy_anniv)
+        player = ExoPlayer.Builder(this).build()
+    }
+
+    fun backToMain(view: View) {
+        finish()
+    }
+
+    fun playASong(view: View){
+        val filePath = "Til Death Do Us Part.mp3"
+        song.playSong(filePath,
+            onSuccess = { audioFileUrl ->
+                // Prepare ExoPlayer with MediaSource
+                val audioUri = Uri.parse(audioFileUrl)
+                // Build the media item.
+                val mediaItem = MediaItem.fromUri(audioUri)
+
+                player.setMediaItem(mediaItem)
+                player.prepare()
+                player.play()
+            },
+            onFailure = { exception ->
+
+            }
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Release the player
+        player.release()
     }
 }
--- app/src/main/java/com/example/birdday/MainActivity.kt
+++ app/src/main/java/com/example/birdday/MainActivity.kt
@@ -13,12 +13,14 @@ class MainActivity : AppCompatActivity() {
         SongStorage(this).initializeFirebaseStorage()
     }
     fun safePlaceCard(view: View) {
-        val intent = Intent(this, SafePlace::class.java)
-        startActivity(intent)
+        val intent1 = Intent(this, SafePlace::class.java)
+        startActivity(intent1)
+        println("Safe Place Card Clicked")
     }
 
     fun happyAnnivCard(view: View) {
-        val intent = Intent(this, HappyAnniv::class.java)
-        startActivity(intent)
+        val intent2 = Intent(this, HappyAnniv::class.java)
+        startActivity(intent2)
+        println("Happy Anniversary Card Clicked")
     }
 }
